66cf7df49c088a7367862b61db3e3c9fc12c1004
Stuart <stuart@stuart-tm2.home>
2017-04-13 10:16:24 -0500

    Added bash scripts for scheduling

---
+++ daily.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/stuart/script_manager
+/usr/bin/python /home/stuart/script_manager/script_manager.py -c tm2.ini -s clear_status twitter_bot
---
+++ running.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/stuart/script_manager
+/usr/bin/python /home/stuart/script_manager/script_manager.py -c tm2.ini twitter_bot
+
