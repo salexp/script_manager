ef09b538300957fdb73124b4486e29cbe642ffe6
Stuart Petty <stuartpetty@MacBook-Pro.local>
2017-06-19 22:18:01 -0500

    Respond to !help

--- util/groupme/groupme.py
+++ util/groupme/groupme.py
@@ -2,6 +2,7 @@ import json
 import requests
 from requests import session
 from group import GMeGroup
+from message import GMeMessage
 from do_not_upload import BOT_ID, GROUP_ID
 
 
@@ -13,6 +14,10 @@ class GMeBot:
         self.uri = "https://api.groupme.com/v3/bots/post"
         self.session = session()
 
+        self.commands = {
+            '!help': self.say_help
+        }
+
         self.last_heard = None
 
     def __post(self, data):
@@ -20,12 +25,17 @@ class GMeBot:
 
     def listen(self, data):
         if data.get('sender_type') != 'bot':
-            self.last_heard = data
-            self.say("Commands:\n!help\n!stats")
+            msg = GMeMessage(data=data, listening=self)
+            self.last_heard = msg
+            if msg.command in self.commands.keys():
+                self.commands.get(msg.command)()
 
     def say(self, text):
         data = {'bot_id': self.bot_id, 'text': text}
         self.__post(data)
+
+    def say_help(self):
+        self.say("Commands:\n!help")
 
 
 class GroupMe:
---
+++ util/groupme/message.py
@@ -0,0 +1,24 @@
+commands = {
+    '!help': 'help',
+}
+
+
+class GMeMessage:
+    def __init__(self, data, listening):
+        self.data = data
+        self.listening = listening
+
+        self._is_command = None
+        self._command = None
+
+    @property
+    def is_command(self):
+        if self._is_command is None:
+            self._is_command = self.data['text'].startswith('!')
+        return self._is_command
+
+    @property
+    def command(self):
+        if self.is_command and self._command is None:
+            self._is_command = self.data['text'].partition(' ')[0]
+        return self._is_command
