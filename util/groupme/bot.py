37dd13445fbc962e1fa602164f7d091165bb719b
Stuart Petty <stuart.petty@garmin.com>
2017-06-27 15:25:37 -0500

    Silence ThugBot for deployment.

--- util/groupme/bot.py
+++ util/groupme/bot.py
@@ -60,7 +60,18 @@ class ThugBot(GMeBot):
         self.db = Database('Fantasy', user='local', password='',
                            host='127.0.0.1', port='3306')
 
+    def say(self, text):
+        pass
+
+
+class TestBot(ThugBot):
+    def __init__(self, bot_id, group_id):
+        ThugBot.__init__(self, bot_id, group_id)
+
         self.add_command('!picks', self.say_picks)
+
+    def say(self, text):
+        GMeBot.say(self, text)
 
     def say_picks(self):
         text = "{}\n{}".format(self.last_heard.sender, ", ".join(['1.1', '2.1', '2.6', '4.1', '5.1']))
