183687b6b3f89d92b22e6c0c2c67b16cf67f392a
Stuart Petty <stuart.petty@garmin.com>
2017-07-18 15:16:34 -0500

    Removed print.

--- util/retries.py
+++ util/retries.py
@@ -10,7 +10,6 @@ def retries(retries_count=0, wait_time=0.0):
             while current_count < max_runs:
                 try:
                     current_count += 1
-                    print current_count
                     ret = func(*args, **kwargs)
                     return ret
                 except Exception as e:
