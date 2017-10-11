79009951987a04480ff51feea51380e5c3356198
Stuart <stuart@stuart-tm2.home>
2017-10-11 13:24:46 -0500

    Check for transactions every minute.

--- minute.sh
+++ minute.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/stuart/script_manager
 /usr/bin/python /home/stuart/script_manager/script_manager.py -c tm2.ini twitter_bot
+/usr/bin/python /home/stuart/script_manager/thug_island_data.py -t
 
